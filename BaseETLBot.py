e29486334336a96feb2bc1458742cd8bf4d9f95f
Alonsole <157732573+Alonsole@users.noreply.github.com>
2024-07-20 00:26:14 +0300

    Uploading a project

---
+++ BaseETLBot.py
@@ -0,0 +1,289 @@
+import sqlalchemy
+import sqlalchemy as sq
+import json
+import random
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import func
+
+Base = declarative_base()
+
+
+class Connectbase:
+    def __init__(self, name, passw, server, port, name_db, sqlm, user_name=None, user_id=None,
+                 new_rus_word=None, new_eng_word=None):
+        self.name = name
+        self.passw = passw
+        self.server = server
+        self.port = port
+        self.database = name_db
+        self.sqlm = sqlm
+        self.user_name = user_name
+        self.user_id = user_id
+        self.new_rus_word = new_rus_word
+        self.new_eng_word = new_eng_word
+        self.DSN = f"{self.sqlm}://{self.name}:{self.passw}@{self.server}:{self.port}/{self.database}"
+
+    def creatdb(self):
+        """Создание базы"""
+        engine = sqlalchemy.create_engine(self.DSN)
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        session.close()
+        return print("Создана база из 3х таблиц")
+
+    def createuser(self):
+        """Создание юзера в базе"""
+        engine = sqlalchemy.create_engine(self.DSN)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        session.add(Users(user_name=self.user_name,
+                          user_id=self.user_id))
+        session.commit()
+        session.close()
+
+    def check_user(self):
+        """проверка наличия пользователя в базе"""
+        engine = sqlalchemy.create_engine(self.DSN)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        query = session.query(Users.user_id)
+        result = query.filter(Users.user_id == self.user_id).all()
+        session.close()
+        return result
+
+    def loaddataword(self):
+        """Загрузка тестовой базы"""
+        engine = sqlalchemy.create_engine(self.DSN)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        for record in data:
+            new_word = Words(
+                id=record.get('id'),
+                eng_word=record.get('word_eng'),
+                rus_word=record.get('word_rus').encode('cp1251').decode('utf-8'))
+            session.add(new_word)
+        session.commit()
+        session.close()
+        return print("Загружена тестовая база слов")
+
+    def random_word(self):
+        engine = sqlalchemy.create_engine(self.DSN)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        subq = session.query(Users.id).filter(Users.user_id == self.user_id)
+        query1 = session.query(Words.rus_word, Words.eng_word).filter(Words.word_user == subq[0][0]).all()
+        query2 = session.query(Words.rus_word, Words.eng_word).filter(Words.word_user == None).all()
+        all_words = query1 + query2
+        random_words = random.sample(all_words, 4)
+        session.close()
+        return random_words
+
+    def add_word(self):
+        try:
+            engine = sqlalchemy.create_engine(self.DSN)
+            Session = sessionmaker(bind=engine)
+            session = Session()
+            query = session.query(Users.id)
+            result = query.filter(Users.user_id == self.user_id).all()[0][0]
+            word_count = session.query(func.max(Words.id)).scalar()
+            new_word = Words(
+                id=word_count + 1,
+                eng_word=self.new_eng_word,
+                rus_word=self.new_rus_word,
+                word_user=result)
+            session.add(new_word)
+            session.commit()
+            session.close()
+            result_add = 0
+            return result_add
+        except:
+            result_add = 1
+            return result_add
+
+    def del_word(self):
+        try:
+            engine = sqlalchemy.create_engine(self.DSN)
+            Session = sessionmaker(bind=engine)
+            session = Session()
+            subq = session.query(Users.id).filter(Users.user_id == self.user_id).scalar()
+            query_to_delete = session.query(Words).filter(Words.word_user == subq,
+                                                          Words.rus_word == self.new_rus_word).first()
+            session.delete(query_to_delete)
+            session.commit()
+            session.close()
+            result = f'Удалено из базы слово : {self.new_rus_word}'
+            return result
+        except:
+            result = f'Вы ещё не добавиляли слово {self.new_rus_word}'
+            return result
+
+    def check_word(self):
+
+        engine = sqlalchemy.create_engine(self.DSN)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        subq = session.query(Users.id).filter(Users.user_id == self.user_id).scalar()
+        query = session.query(Words).filter(Words.word_user == subq,
+                                            Words.rus_word == self.new_rus_word).count()
+        session.close()
+        return query
+
+    def my_word(self):
+        engine = sqlalchemy.create_engine(self.DSN)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        subq = session.query(Users.id).filter(Users.user_id == self.user_id)
+        query = session.query(Words.rus_word).filter(Words.word_user == subq[0][0]).all()
+        result = []
+        for my_word_information in query:
+            result.append(my_word_information[0])
+        session.close()
+        if not result:
+            result = "Вы не добавили слов"
+            return result
+        else:
+            return result
+
+    def experience_user(self):
+        try:
+            engine = sqlalchemy.create_engine(self.DSN)
+            Session = sessionmaker(bind=engine)
+            session = Session()
+            query = session.query(Users.id)
+            result = query.filter(Users.user_id == self.user_id).all()[0][0]
+            word_count = session.query(Experience.id).count()
+            word_exp = Experience(
+                id=word_count + 1,
+                eng_word_exp=self.new_eng_word,
+                user_exp=result)
+            session.add(word_exp)
+            session.commit()
+            session.close()
+        except:
+            pass
+
+    def get_experience_user(self):
+        engine = sqlalchemy.create_engine(self.DSN)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        subq = session.query(Users.id).filter(Users.user_id == self.user_id)
+        query = session.query(Experience).filter(Experience.user_exp == subq[0][0]).count()
+        session.close()
+        return query
+
+
+with open('dataword.json', 'r') as f:
+    data = json.load(f)
+
+
+class Users(Base):
+    __tablename__ = "users"
+    id = sq.Column(sq.Integer, primary_key=True)
+    user_name = sq.Column(sq.String(length=250), unique=True)
+    user_id = sq.Column(sq.String(length=20), unique=True)
+
+
+class Words(Base):
+    __tablename__ = "words"
+    id = sq.Column(sq.Integer, primary_key=True)
+    eng_word = sq.Column(sq.String(length=250), unique=False)
+    rus_word = sq.Column(sq.String(length=250), unique=False)
+    word_user = sq.Column(sq.Integer, sq.ForeignKey("users.id"))
+    users = relationship("Users", backref="words")
+
+
+class Experience(Base):
+    __tablename__ = "experience"
+    id = sq.Column(sq.Integer, primary_key=True)
+    eng_word_exp = sq.Column(sq.String(length=250), unique=False)
+    user_exp = sq.Column(sq.Integer, sq.ForeignKey("users.id"))
+    users = relationship("Users", backref="experience")
+
+
+"""подключается к БД любого типа на ваш выбор"""
+name = "postgres"
+passw = 'Пароль'
+server = "localhost"
+port = "Порт"
+name_db = "Указать базу"
+sqlm = "postgresql"
+
+
+def createdatabase():
+    """создание базы"""
+    result = Connectbase(name, passw, server, port, name_db, sqlm).creatdb()
+    return result
+
+
+def loadworddatabase():
+    """заливка базы"""
+    result = Connectbase(name, passw, server, port, name_db, sqlm).loaddataword()
+    return result
+
+
+def registration(user_name, user_id):
+    """регистрация пользователя"""
+    Connectbase(name, passw, server, port, name_db, sqlm, user_name,
+                str(user_id)).createuser()
+
+
+def check_registration(user_id):
+    """проверка регистрации"""
+    result = Connectbase(name, passw, server, port, name_db, sqlm, None,
+                         str(user_id)).check_user()
+    if not result:
+        return None
+    else:
+        return result[0]
+
+
+def get_random_word(user_id):
+    """получить 4 случайных пары слов"""
+    result = Connectbase(name, passw, server, port, name_db, sqlm, None,
+                         str(user_id)).random_word()
+    return result
+
+
+def add_word_db(id_user_word, new_rus_word, new_eng_word):
+    """Добавить слово"""
+    print(new_rus_word)
+    result = Connectbase(name, passw, server, port, name_db, sqlm, None,
+                         str(id_user_word), new_rus_word, new_eng_word).add_word()
+    return result
+
+
+def del_word_db(user_id, new_rus_word):
+    """Удаление слова"""
+    print(new_rus_word)
+    result = Connectbase(name, passw, server, port, name_db, sqlm, None,
+                         str(user_id), new_rus_word).del_word()
+    return result
+
+
+def get_my_word(user_id):
+    """Получить список моих слов"""
+    result = Connectbase(name, passw, server, port, name_db, sqlm, None,
+                         str(user_id)).my_word()
+    return result
+
+
+def add_experience(id_user_word, new_eng_word):
+    """Добавить изученное слово"""
+    result = Connectbase(name, passw, server, port, name_db, sqlm, None,
+                         str(id_user_word), None, new_eng_word).experience_user()
+    return result
+
+
+def get_experience(id_user_word):
+    """Добавить изученное слово"""
+    result = Connectbase(name, passw, server, port, name_db, sqlm, None,
+                         str(id_user_word), None, None).get_experience_user()
+    return result
+
+
+def get_check_word(id_user_word, new_rus_word):
+    result = Connectbase(name, passw, server, port, name_db, sqlm, None,
+                         str(id_user_word), new_rus_word, None).check_word()
+    return result
---
+++ ETLBot.py
@@ -0,0 +1,249 @@
+import random
+import telebot
+from telebot import types
+from telebot.handler_backends import StatesGroup, State
+import BaseETLBot
+
+TOKEN = 'TOKEN'
+bot = telebot.TeleBot(TOKEN)
+
+
+class Addword:
+    id_user_word = None
+    new_rus_word = None
+    new_eng_word = None
+    id_add_word = 0
+
+
+class Command:
+    """Все кнопки тут"""
+    registation = types.KeyboardButton("Зарегистрироваться 🆗")
+    next_word = types.KeyboardButton("Далее ⏩")
+    add_word = types.KeyboardButton("Добавить слово ➕")
+    my_word = types.KeyboardButton("Мои слова ✍")
+    help_b = types.KeyboardButton("Помощь 💬")
+    btn_start = types.KeyboardButton("Запуск 🆗")
+    back = types.KeyboardButton("Назад ⏪")
+    my_experience = types.KeyboardButton("Статистика обучения 🏆")
+
+
+class MyStates(StatesGroup):
+    target_word = State()
+    translate_word = State()
+    another_words = State()
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    global user_info
+    user_info = message.from_user.first_name
+    """Приветствие"""
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    user_id = message.from_user.id  # передаю в базу ид пользователя
+    check_registration = BaseETLBot.check_registration(user_id)  # запускаю проверку пользователя
+    if check_registration is None:  # если пользователя нет = создаём
+        btn_first = types.KeyboardButton(Command.registation.text)
+    else:
+        btn_first = types.KeyboardButton(Command.btn_start.text)
+    markup.add(btn_first, Command.help_b)
+    bot.reply_to(message, f"Здравствуйте, уважаемый {user_info} 👋 \n"
+                          f"Бот создан с целью практических занятий английскому языку. \n"
+                          "Тренировки можете проходить в удобном для Вас темпе и в любое удобное вермя ⌚. \n"
+                          "У Вас есть возможность добавлять личные слова, которые будут доступны только Вам.\n"
+                          "Вы сможете собрать свою собственную базу для обучения 💎.  \n"
+                          "Для продолжения используйте меню ⬇️", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: message.text == Command.help_b.text)
+def help_message(message):
+    """Всё просто это HELP"""
+    bot.reply_to(message, f"Описание возможностей и функций кнопок 📑\n"
+                          f"Первые четыре кнопки отображают варианты перевода слова, который Бот Вам "
+                          f"предложил. Только один верный вариант 🎲. "
+                          f"Не беспокойтесь, попытки неограничены 💪, а также Вы можете пропустить слово ⏩\n"
+                          f"| {Command.next_word.text} | используйте для перехода к следующей странице слов.\n"
+                          f"| {Command.add_word.text} | добавляет слово на 🇷🇺 и 🏴󠁧󠁢󠁥󠁮󠁧󠁿 языке"
+                          "в вашу личную базу знаний.\n"
+                          f"| {Command.my_word.text} | показать список добавленных Вами слов \n"
+                          f"| {Command.my_experience.text} | вывод статистики успешных сопоставлений слов\n"
+                          f"Внимание ‼ Добавляйте слова аккуратно и указывайте верный перевод \n"
+                          f"Если Вы случайно создали ошибочное слово 🙁 - Удалите его ‼\n"
+                          f"Добавленные слова доступны только пользователю добавившему слово 😜")
+
+
+@bot.message_handler(func=lambda message: message.text == Command.registation.text)
+def registartion(message):
+    """Регистрация нового юзера для учёта слов"""
+    user_id = message.from_user.id  # передаю в базу ид пользователя
+    BaseETLBot.registration(user_info, user_id)
+    bot.reply_to(message, f"Спасибо 🤝, {user_info} за регистрацию и внимание к данному Боту 🛸")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(Command.btn_start, Command.help_b)
+    bot.send_message(message.chat.id, text=f"{user_info}. Для продолжения нажмите Запуск ⚡", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: message.text == Command.btn_start.text)
+def start_of_studies(message):
+    """Пользователь зарегистрировался и стартовал!"""
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    user_id = message.from_user.id
+    random_word = BaseETLBot.get_random_word(user_id)
+    rus_word = random_word[0][0]
+    global eng_word
+    eng_word = random_word[0][1]
+    wrong_word_one = random_word[1][1]
+    wrong_word_two = random_word[2][1]
+    wrong_word_three = random_word[3][1]
+    buttons = [eng_word, wrong_word_one, wrong_word_two, wrong_word_three]
+    random.shuffle(buttons)
+    markup.add(*buttons,
+               Command.next_word,
+               Command.add_word,
+               Command.my_word,
+               Command.my_experience,
+               Command.help_b)
+    message_question = ["Выберите верный перевод слова ?",
+                        "Выберите правильный перевод слова ?",
+                        "Какой из перечисленных вариантов перевода слова является правильным ?",
+                        "Как правильно переводится слово ?",
+                        "Вам нужно выбрать один вариант перевода слова:",
+                        "Попробуйте угадать верный перевод слова:",
+                        "Вы знаете верный перевод слова?,"
+                        "Нажмите на кнопку с верным переводом слова."
+                        "Угадаете перевод с первой попытки?"]
+    random.shuffle(message_question)
+    bot.send_message(message.chat.id, f"{message_question[0]} \n ⭐ {rus_word} ⭐:", reply_markup=markup)
+    bot.set_state(message.from_user.id, MyStates.target_word, message.chat.id)
+    global data
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['rus_word'] = rus_word
+        data['eng_word'] = eng_word
+        data['other_words'] = [wrong_word_one, wrong_word_two, wrong_word_three]
+
+
+@bot.message_handler(func=lambda message: message.text == Command.add_word.text)
+def add_word(message):
+    """Добавляю слова"""
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = [Command.back]
+    markup.add(*buttons)
+    Addword.id_user_word = message.from_user.id
+    if Addword.id_add_word == 0:
+        bot.send_message(message.chat.id, f"Для добавления слова Вам необходимо ввести слово и его перевод. \n"
+                                          f"Ввод слова производите с заглавной буквы",
+                         reply_markup=markup)
+        bot.send_message(message.chat.id, f"Введите слово на русском языке "
+                                          f"и отправьте сообщение: 📝")
+    else:
+        bot.send_message(message.chat.id, f"Введите перевод слова на английском языке и "
+                                          f"отправьте сообщение: 📝", reply_markup=markup)
+    bot.register_next_step_handler(message, send_add_word)
+
+
+def send_add_word(message):
+    """Заливка нового слова"""
+    if message.text == Command.back.text:
+        Addword.id_add_word = 0
+        start_of_studies(message)
+    elif Addword.id_add_word == 0:
+        Addword.new_rus_word = message.text
+        result = BaseETLBot.get_check_word(Addword.id_user_word, Addword.new_rus_word)
+        if result > 0:
+            bot.send_message(message.chat.id, f"Слово 🇷🇺 {Addword.new_rus_word} уже есть в базе знаний ")
+            add_word(message)
+        else:
+            Addword.id_add_word = 1
+            add_word(message)
+    else:
+        Addword.new_eng_word = message.text
+        Addword.id_add_word = 0
+        BaseETLBot.add_word_db(Addword.id_user_word, Addword.new_rus_word, Addword.new_eng_word)
+        bot.send_message(message.chat.id, f"Успешно добалено слово 🇷🇺 {Addword.new_rus_word} и \n"
+                                          f"перевод 🏴󠁧󠁢󠁥󠁮󠁧󠁿󠁧󠁢󠁥󠁮󠁧󠁿󠁧󠁢󠁥󠁮󠁧󠁿 󠁧󠁢󠁥󠁮󠁧󠁿{Addword.new_eng_word}")
+
+
+@bot.message_handler(func=lambda message: message.text == Command.back.text)
+def send_cancel_word(message):
+    """Возврат"""
+    start_of_studies(message)
+
+
+@bot.message_handler(func=lambda message: message.text == Command.my_word.text)
+def my_word_list(message):
+    """Получаю список своих слов"""
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = [Command.back]
+    markup.add(*buttons)
+    bot.send_message(message.chat.id, f"Вот Список Добавленных Вами слов: "
+                                      f"📜{BaseETLBot.get_my_word(message.from_user.id)}📜", reply_markup=markup)
+    bot.send_message(message.chat.id, f"Для удаления введите слово из списка и отправьте сообщение")
+    bot.register_next_step_handler(message, del_word)
+
+
+@bot.message_handler(func=lambda message: message.text == Command.my_experience.text)
+def my_experience(message):
+    """Очищаю data"""
+    del data['eng_word']
+
+    """Отображение статистики изучения слов"""
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = [Command.back]
+    markup.add(*buttons)
+    bot.send_message(message.chat.id,
+                     f"Ваш прогресс изучения слов - "
+                     f"{BaseETLBot.get_experience(message.from_user.id)} 🥳 (количество изученных слов)",
+                     reply_markup=markup)
+
+
+def del_word(message):
+    """Удаление слов или назад в меню"""
+    if message.text == Command.back.text:
+        start_of_studies(message)
+    else:
+        new_rus_word = message.text
+        bot.send_message(message.chat.id, BaseETLBot.del_word_db(message.from_user.id, new_rus_word))
+
+
+@bot.message_handler(func=lambda message: message.text == Command.next_word.text)
+def next_word(message):
+    """Загружаю новый лист слов"""
+    start_of_studies(message)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def message_reply(message):
+    """Проверка решения"""
+    Addword.id_user_word = message.from_user.id
+    Addword.new_eng_word = message.text
+    message_win = ["А Вы молодец 🧡. Верно ✅",
+                   "У Вас отлично выходит 🧡. Правильно ✅",
+                   "Вы великолепны 🧡. Так точно ✅",
+                   "Вы бесспорно правы ✅",
+                   "Безупречно 🧡. Угадали ✅"]
+    random.shuffle(message_win)
+
+    message_lose = ["Увы ошибка 🙁, но не расстраивайтесь и попробуйте ещё раз ✅",
+                    "Ошиблись 🙄, повторите попытку ✅ или нажмите далее ⏩",
+                    "Вы были очень близки к верному переводу 🙃",
+                    "Попытка не пытка 😘. Продолжайте ✅",
+                    "Ой Ой, чуть чуть не так. Ещё раз? ✅"]
+    random.shuffle(message_lose)
+    try:
+        if message.text == data['eng_word']:
+            bot.send_message(message.chat.id, f"{message_win[0]}")
+            bot.send_message(message.chat.id, f"Продолжаем ⏩⏩⏩")
+            BaseETLBot.add_experience(Addword.id_user_word, Addword.new_eng_word)
+            start_of_studies(message)
+        else:
+            bot.send_message(message.chat.id, f"{message_lose[0]}")
+    except:
+        start_of_studies(message)
+
+
+if __name__ == '__main__':
+    """Создание базы, заливка базы и запуск бота"""
+    # 1. BaseETLBot.createdatabase()
+    # 2. BaseETLBot.loadworddatabase()
+
+    # print('Бот запущен...')
+    # 3. bot.polling()
